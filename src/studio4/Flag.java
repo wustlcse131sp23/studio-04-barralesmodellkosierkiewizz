03864c460ec8083d901df6cada725843f22823f7
ximenabarrales <ximenabarrales@ximenas-macbook.dhcp.wustl.edu>
2023-02-23 14:13:54 -0600

    Finished studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,28 @@
 package studio4;
 
 import java.awt.Color;
+import java.io.File;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
 		
+
+		StdDraw.setPenColor(120, 186, 45);
+		StdDraw.filledRectangle(0.5, 0.5,1,.2);
+		
+		StdDraw.setPenColor(Color.PINK);
+		StdDraw.filledRectangle(0.5, 0.5,.2,.2);
+		
+		StdDraw.setPenColor(173, 0, 228);
+		StdDraw.filledCircle(0.5, 0.5,.2);
+
+		StdDraw.setPenColor(0,0,0);
+
+		StdDraw.rectangle(.5, 0.5, 1, 0.2);				
 	}
 }
